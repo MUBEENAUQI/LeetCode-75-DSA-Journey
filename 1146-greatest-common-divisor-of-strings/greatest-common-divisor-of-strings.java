bd5300333473a1c81578ec337748061a6fd140f3
MUBEENAUQI <49668351+MUBEENAUQI@users.noreply.github.com>
2024-11-26 10:33:46 +0530

    Time: 1 ms (67.34%) | Memory: 42.5 MB (15.93%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+
+        if((str1+str2).equals(str2+str1)){
+        
+        int l1 = str1.length();
+        int l2 = str2.length();
+        int max =0;
+
+        for( int i=1;i<=l2;i++){
+            if(l1%i==0 && l2%i==0){
+                max = i;
+            }
+        }
+        return str2.substring(0,max);
+    }
+        else{
+            return "";
+        }
+    }
+}
