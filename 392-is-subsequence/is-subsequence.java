a3cdb3e867864b88b9369021586d18f81c17ad4e
MUBEENAUQI <49668351+MUBEENAUQI@users.noreply.github.com>
2024-12-30 13:29:53 +0530

    Time: 5 ms (3.87%) | Memory: 41.9 MB (11.54%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,37 +1,26 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int sPointer = 0, tPointer = 0;
 
-        // Traverse both strings
-        while (sPointer < s.length() && tPointer < t.length()) {
-            if (s.charAt(sPointer) == t.charAt(tPointer)) {
-                sPointer++; // Move sPointer when characters match
+        int l = t.length();
+        int l1 = s.length();
+        int j = 0,f=0;
+        if (l1 ==0)
+        return true;
+        for(int i=0;i<l;i++){
+            if(t.charAt(i)==s.charAt(j)){
+                System.out.println("i= "+i+ " j= "+j );
+                j++;
+                if (j==l1){
+                    f=1;
+                    break;
+                }
             }
-            tPointer++; // Always move tPointer
-        }
-
-        // If sPointer has reached the end of s, all characters were matched
-        return sPointer == s.length();
-        // int l = t.length();
-        // int l1 = s.length();
-        // int j = 0,f=0;
-        // if (l1 ==0)
-        // return true;
-        // for(int i=0;i<l;i++){
-        //     if(t.charAt(i)==s.charAt(j)){
-        //         System.out.println("i= "+i+ " j= "+j );
-        //         j++;
-        //         if (j==l1){
-        //             f=1;
-        //             break;
-        //         }
-        //     }
             
-        // }
-        // if(f==1)
-        // return true;
-        // else
-        // return false;
+        }
+        if(f==1)
+        return true;
+        else
+        return false;
         
     }
 }
