c27c80492da657bf15c2f0492477e11eb8522a89
MUBEENAUQI <49668351+MUBEENAUQI@users.noreply.github.com>
2025-01-05 00:26:09 +0530

    Time: 11 ms (86.71%) | Memory: 45 MB (29.86%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,94 +1,55 @@
 class Solution {
     public int maxVowels(String s, int k) {
+
         int len = s.length();
-        int maxVowels = 0, currentVowels = 0;
-
-        // Count vowels in the first window (first k characters)
-        for (int i = 0; i < k; i++) {
-            if (isVowel(s.charAt(i))) {
-                currentVowels++;
+        int l=0,r=0,f=0,max=0;
+        
+        while(r!=len){
+            char c = s.charAt(r);
+            if(r<k){
+                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' )
+                f++;
+                r++;
+            }else{
+                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' )
+                f++;
+                c = s.charAt(l);
+                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' )
+                f--;
+                l++;
+                r++;
             }
+            if(f>max)
+            max = f;
+            if(max ==k)
+            break;
         }
+        return max;
 
-        maxVowels = currentVowels;
+        // time exceeded
+        // int len = s.length();
+        // int f=0,max=0;;
+        // int l=0,r=k;
+        // String tmp ="";
 
-        // Slide the window across the string
-        for (int i = k; i < len; i++) {
-            // Remove the effect of the character going out of the window
-            if (isVowel(s.charAt(i - k))) {
-                currentVowels--;
-            }
+        // while(r!=len+1){
+        //     tmp = s.substring(l,r);
+        //     // System.out.println("temp= "+tmp);
+        //     // System.out.println("l= "+l+" r= "+r);
 
-            // Add the effect of the character coming into the window
-            if (isVowel(s.charAt(i))) {
-                currentVowels++;
-            }
+        //     for(int i =0;i<tmp.length();i++){
+        //         char c = tmp.charAt(i);
+        //          if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') 
+        //        f++;
+        //     }
 
-            // Update the maxVowels if necessary
-            maxVowels = Math.max(maxVowels, currentVowels);
-        }
-
-        return maxVowels;
-    }
-
-    // Helper function to check if a character is a vowel
-    private boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        //     if(f>max)
+        //     max=f;
+        //     System.out.println("f = "+f);
+        //     f=0;
+        //     l++;
+        //     r++;
+        // }
+        // return max;
     }
 }
-
-// class Solution {
-//     public int maxVowels(String s, int k) {
-
-//         int len = s.length();
-//         int l=0,r=0,f=0,max=0;
-        
-//         while(r!=len){
-//             char c = s.charAt(r);
-//             if(r<k){
-//                 if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' )
-//                 f++;
-//                 r++;
-//             }else{
-//                 if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' )
-//                 f++;
-//                 c = s.charAt(l);
-//                 if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' )
-//                 f--;
-//                 l++;
-//                 r++;
-//             }
-//             if(f>max)
-//             max = f;
-//             if(max ==k)
-//             break;
-//         }
-//         return max;
-
-//         // time exceeded
-//         // int len = s.length();
-//         // int f=0,max=0;;
-//         // int l=0,r=k;
-//         // String tmp ="";
-
-//         // while(r!=len+1){
-//         //     tmp = s.substring(l,r);
-//         //     // System.out.println("temp= "+tmp);
-//         //     // System.out.println("l= "+l+" r= "+r);
-
-//         //     for(int i =0;i<tmp.length();i++){
-//         //         char c = tmp.charAt(i);
-//         //          if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') 
-//         //        f++;
-//         //     }
-
-//         //     if(f>max)
-//         //     max=f;
-//         //     System.out.println("f = "+f);
-//         //     f=0;
-//         //     l++;
-//         //     r++;
-//         // }
-//         // return max;
-//     }
-// }
